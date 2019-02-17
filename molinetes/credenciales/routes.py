e421006da9221d63fef45897744974a11d9f850e
nuxion <nuxion@gmail.com>
2019-02-17 19:24:06 -0300

    crud: credenciales

--- molinetes/credenciales/routes.py
+++ molinetes/credenciales/routes.py
@@ -1,9 +1,84 @@
 from flask import (
     Blueprint, g, redirect, request, session, url_for, jsonify )
+from molinetes.models import Credencial, db
 
 bp = Blueprint('credenciales', __name__)
 
 @bp.route('/credenciales', methods=['GET'])
-def simple_response():
-    return jsonify({'msg': 'credenciales'})
+def list_all():
+
+    status = 200
+    credenciales = Credencial.query.all()
+    res = [ {'name': c.nombre,
+             'id': c.id } for c in credenciales ]
+
+    return jsonify(res), status
+
+@bp.route('/credenciales/<c_id>', methods=['GET'])
+def list_one(c_id):
+
+    status = 200
+    c = Credencial.query.filter(Credencial.id==c_id).first()
+    if c:
+      res = { 'name': c.nombre, 'id': c.id }
+    else:
+        res = { 'msg': 'empty' }
+        status = 204
+
+    return jsonify(res), status
+
+@bp.route('/credenciales', methods=['POST'])
+def save_credenciales():
+
+    if request.is_json:
+        data = request.get_json()
+        try:
+            credencial  = Credencial(nombre = data['nombre'])
+            db.session.add(credencial)
+            db.session.commit()
+            status = 201
+            msg = "Its ok"
+        except KeyError:
+            msg = 'Params error'
+            status = 400
+    else:
+        msg =  "Must to be json"
+        status = 415
+
+    return jsonify({'msg': msg }), status
+
+@bp.route('/credenciales', methods=['PUT'])
+def update():
+
+    if request.is_json:
+
+        data = request.get_json()
+        c = Credencial.query.filter(Credencial.id==data['id']).first()
+        if c:
+            c.nombre = data['nombre']
+            db.session.commit()
+        else:
+            status = 204
+
+
+    res = {'name': c.nombre,
+             'id': c.id }
+
+    return jsonify(res)
+
+@bp.route('/credenciales/<c_id>', methods=['DELETE'])
+def delete(c_id):
+    status = 200
+
+    c = Credencial.query.filter(Credencial.id==c_id).first()
+    if c:
+        db.session.delete(c)
+        db.session.commit()
+        msg = {'msg': '{} deleted'.format(c_id)}
+    else:
+        status = 204
+        msg = {'msg': 'No Content'}
+
+    return jsonify(msg), status
+
 
--- tests/test_credenciales.py
+++ tests/test_credenciales.py
@@ -1,11 +1,21 @@
 import pytest
 
 
-def test_root_page(test_client):
+def test_list_credenciales(test_client, init_db):
     """
     GIVEN a flask app
     WHEN the '/credenciales' page is requested
     """
     response = test_client.get('/credenciales')
     assert response.status_code == 200
-    assert response.get_json() == { 'msg': 'credenciales' }
+    assert len(response.get_json()) == 2
+
+def test_post_credenciales(test_client, init_db):
+    """
+    GIVEN a flask app
+    WHEN the '/credenciales' page is requested
+    """
+    response_post = test_client.post('/credenciales', json={'nombre':'test'})
+    response_get = test_client.get('/credenciales')
+    assert response_post.status_code == 201
+    assert len(response_get.get_json()) == 3
