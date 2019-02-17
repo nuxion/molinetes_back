d0ad6b1cdf8bccc4aecacab2880cac5b6550e0d2
nuxion <nuxion@gmail.com>
2019-02-17 20:17:20 -0300

    models: backref to event

--- molinetes/models/eventos.py
+++ molinetes/models/eventos.py
@@ -8,6 +8,8 @@ class Evento(db.Model):
     molinetes = db.relationship('Molinete',
                                 backref='evento',
                                 lazy=True)
+    credenciales = db.relationship('Credencial',
+                                backref='evento')
 
     def __repr__(self):
         return '<Evento %r>' % self.nombre
