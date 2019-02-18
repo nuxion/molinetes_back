96a7e46bc05ff8df3972ecea7b863459986d87b0
nuxion <nuxion@gmail.com>
2019-02-18 18:24:12 -0300

    cli: inserts mock data on db

--- cli.py
+++ cli.py
@@ -2,6 +2,7 @@
 #   env FLASK_APP="cli.py" flask db init
 #
 import os
+import click
 from molinetes import create_app
 from molinetes.models import db, Evento, Molinete, Credencial
 from flask_migrate import Migrate
@@ -18,5 +19,26 @@ def make_shell_context():
                 Evento=Evento,
                 Molinete=Molinete
                 )
+
+@app.cli.command()
+def populate_db():
+    """Generate data."""
+
+    # Crea las credenciales
+    [ db.session.add(Credencial(nombre=x))
+     for x in ['xavier', 'pepe', 'juan', 'rocio', 'julieta']]
+
+    # Crea dos eventos
+    db.session.add(Evento(nombre='ACDC', cod='20180701'))
+    db.session.add(Evento(nombre='BocaVsRiver', cod='20170701'))
+
+    db.session.commit()
+
+    # Crea 3 molinetes dos asociados a un evento y 1 a otro.
+    db.session.add(Molinete(evento_cod='20180701'))
+    db.session.add(Molinete(evento_cod='20180701'))
+    db.session.add(Molinete(evento_cod='20170701'))
+    db.session.commit()
+
 
 
